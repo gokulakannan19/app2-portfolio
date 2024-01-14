b5372b2caf51c0f84300400c8d403105bd26007d
gokulakannan <gokulakannan.r@idp.com>
2024-01-14 13:16:06 +0530

    added navigation bar and home page

--- Home.py
+++ Home.py
@@ -11,9 +11,11 @@ with col1:
 with col2:
     st.title("Teacher")
     content = """
-    sdfdsffffffffffffffffffffffffffffffffffffffffjnkkkkkksdkjfbsdujfdsjknfsdjkbfuisdbfjksdbfjksdbfkdsjbfkjsdnfdsf
-    dfnsdkjfnsdlnfjkdsnflsdnfkjdsnfdsuhfiusdfnkjdwfnwiunfdwfusfbds
-    sdlfnsdoifniosdfnsd
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     """
     st.info(content)
 
